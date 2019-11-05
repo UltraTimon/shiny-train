a4afb39c88a482c8fee1647a1bbbdba6519b4519
Timon Bestebreur <timonbestebreur@gmail.com>
2019-11-05 15:36:31 +0100

    Add struct node and method for allocating new node

--- linkedlist.c
+++ linkedlist.c
@@ -2,4 +2,63 @@
 #include "linkedlist.h"
 #include <string.h>
 
-// Add code here
+typedef struct Node Node;
+
+struct Node {
+	int value;
+	Node * next;
+};
+
+Node * newNode(int value) {
+	Node * newNode = malloc(sizeof(int) + sizeof(Node *));
+	newNode->value = value;
+	newNode->next = NULL;
+
+	return newNode;
+}
+
+void add(int i)
+{
+	
+}
+
+void addFirst(int i)
+{
+
+}
+
+void addLast(int i)
+{
+
+}
+
+int get(int index)
+{
+
+}
+
+int getFirst()
+{
+
+}
+
+int getLast()
+{
+
+}
+
+int remove(int index)
+{
+
+}
+
+int removeFirst()
+{
+
+}
+
+int removeLast()
+{
+
+}
+
--- linkedlist.h
+++ linkedlist.h
@@ -1,6 +1,23 @@
 #ifndef __TIMESTWO
 #define __TIMESTWO
 
+typedef struct Node Node;
 
+struct Node {
+	int value;
+	struct Node * next;
+};
+
+void add(int i);
+void addFirst(int i);
+void addLast(int i);
+
+int get(int index);
+int getFirst();
+int getLast();
+
+int remove(int index);
+int removeFirst();
+int removeLast();
 
 #endif
